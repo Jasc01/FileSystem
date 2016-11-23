4428c91f74c5d8c9320c8136b291a14ef04bf104
Cgironalas <cgironalas@gmail.com>
2016-11-23 13:40:44 -0600

    Cambios al gui

--- src/main/MainGUI.java
+++ src/main/MainGUI.java
@@ -26,10 +26,10 @@ public class MainGUI {
 					_fileSystem.create(secAmount, secSize);
 					exit = true;	
 				} catch(NumberFormatException e) {
-					System.out.println("Ivalid parameter");
+					System.err.println("Ivalid parameter");
 				}
 			} else {
-				System.out.println("Command not found");
+				System.err.println("Command not found");
 			}
 		}
 	}
@@ -42,40 +42,44 @@ public class MainGUI {
 		return inputArray;
 	}
 		
-	public void help () {
-		System.out.println("\n\nHELP:");
-		System.out.println("file /name /extension /content  \t | Creates new file in the current directory");
-		System.out.println("makedir /name  \t\t\t\t | Creates new directory inside the current one");
-		System.out.println("cdir /path \t\t\t\t | Changes the current directory");
-		System.out.println("listdir \t\t\t\t | Lists the contents of the directory");
-		System.out.println("modfile /path \t\t\t\t | Modifies the contents of the file given the path");
-		System.out.println("showpro /path \t\t\t\t | Displays the properties of the file in the given path");
-		System.out.println("show /path \t\t\t\t | Displays the file in the given path");
-		System.out.println("copyrv /rpath /vpath \t\t\t | Copies a real file into the virtual path given");
-		System.out.println("copyvr /vpath /rpath \t\t\t | Copies a virtual file into the real path given");
-		System.out.println("copyvv /vpath /vpath \t\t\t | Copies a virtual file into the virtual path given");
-		System.out.println("move /filename|directory /path \t\t | Moves file to destination");
-		System.out.println("remove /filename+|directory \t\t | Removes the specified file");
-		System.out.println("find /filename|directory \t\t | Searches for a file with said name");
-		System.out.println("tree \t\t\t\t\t | Displays the directory tree");
-		System.out.println("exit \t\t\t\t\t | Ends the program\n\n");
+	public void help (String[] pInputArray) {
+	  if(pInputArray.length == 1){
+	    System.out.println("\n\nHELP:");
+        System.out.println("file /name /extension /content  \t | Creates new file in the current directory");
+        System.out.println("makedir /name  \t\t\t\t | Creates new directory inside the current one");
+        System.out.println("cdir /path \t\t\t\t | Changes the current directory");
+        System.out.println("listdir \t\t\t\t | Lists the contents of the directory");
+        System.out.println("modfile /path \t\t\t\t | Modifies the contents of the file given the path");
+        System.out.println("showpro /path \t\t\t\t | Displays the properties of the file in the given path");
+        System.out.println("show /path \t\t\t\t | Displays the file in the given path");
+        System.out.println("copyrv /rpath /vpath \t\t\t | Copies a real file into the virtual path given");
+        System.out.println("copyvr /vpath /rpath \t\t\t | Copies a virtual file into the real path given");
+        System.out.println("copyvv /vpath /vpath \t\t\t | Copies a virtual file into the virtual path given");
+        System.out.println("move /(filename|directory) (/newName)? \t | Moves file to destination");
+        System.out.println("remove /(filename+|directory) \t\t | Removes the specified file");
+        System.out.println("find /(filename|directory) \t\t | Searches for a file with said name");
+        System.out.println("tree \t\t\t\t\t | Displays the directory tree");
+        System.out.println("exit \t\t\t\t\t | Ends the program\n\n");
+	  } else {
+	    System.err.println("Invalid parameters");
+	  }
 	}
 	public void file (String[] pInputArray) {
 		if(pInputArray.length == 4) {
 			if(_fileSystem.createFile(pInputArray[1], pInputArray[2], pInputArray[3], false)) { //file nombre extensión contenido
 				System.out.println("File created");
 			} else { 
-			  System.out.println("File already exists, do you want to overwrite it? (y/n)");
+			  System.err.println("File already exists, do you want to overwrite it? (y/n)");
               String newInput = _input.nextLine();
               if(newInput.toLowerCase().equals("y")){
                 _fileSystem.createFile(pInputArray[1],pInputArray[2], pInputArray[3], true);
                 System.out.println("File created");
               } else {
-                System.out.println("Error creating file");
+                System.err.println("Error creating file");
               }
 			}
 		} else {
-			System.out.println("Invalid parameter");
+		    System.err.println("Invalid parameter(s)");
 		}
 	}
 	public void makedir (String[] pInputArray){
@@ -83,17 +87,17 @@ public class MainGUI {
 			if(_fileSystem.createDirectory(pInputArray[1], false)) { //makedir nombre
 				System.out.println("Directory created");
 			} else {
-				System.out.println("Directory already exists, do you want to overwrite it? (y/n)");
+				System.err.println("Directory already exists, do you want to overwrite it? (y/n)");
 				String newInput = _input.nextLine();
 				if(newInput.toLowerCase().equals("y")){
 				  _fileSystem.createDirectory(pInputArray[1], true);
 				  System.out.println("Directory created");
 				} else {
-				  System.out.println("Error creating directory");
+				  System.err.println("Error creating directory");
 				}
 			}
 		} else {
-			System.out.println("Invalid parameter");
+		    System.err.println("Invalid parameter(s)");
 		}
 	}
 	public void cdir (String[] pInputArray){
@@ -101,10 +105,10 @@ public class MainGUI {
 			if(_fileSystem.changeDirectory(pInputArray[1])) { //cdir path (absoluto o relativo)
 				System.out.println("Directory changed");
 			} else {
-				System.out.println("Error changing directory - Probably doesn't exist");
+				System.err.println("Error changing directory - Probably doesn't exist");
 			}
 		} else {
-			System.out.println("Invalid parameter");
+		    System.err.println("Invalid parameter(s)");
 		}
 	
 	}
@@ -112,7 +116,7 @@ public class MainGUI {
 		if(pInputArray.length == 1) {
 			_fileSystem.listCurrentDirectory(); //listdir
 		} else {
-			System.out.println("Invalid parameter");
+		    System.err.println("Invalid parameter(s)");
 		}
 	}
 	public void modFile(String[] pInputArray) {
@@ -123,64 +127,67 @@ public class MainGUI {
 				if(_fileSystem.modFile(pInputArray[1], newContent)) {
 					System.out.println("File changed");
 				} else {
-					System.out.println("Error modifying file");
+					System.err.println("Error modifying file");
 				}
 			} else {
-				System.out.println("Error modifying file - Probably doesn't exist");
+				System.err.println("Error modifying file - Probably doesn't exist");
 			}
 		} else {
-			System.out.println("Invalid parameter");
+		    System.err.println("Invalid parameter(s)");
 		}
 	}
 	public void showpro (String[] pInputArray) {
 		if(pInputArray.length == 2) {
 			if(_fileSystem.showProperties(pInputArray[1])) { //cdir path (absoluto o relativo)
 			} else {
-				System.out.println("Error showing file properties - Probably doesn't exist");
+				System.err.println("Error showing file properties - Probably doesn't exist");
 			}
 		} else {
-			System.out.println("Invalid parameter");
+		    System.err.println("Invalid parameter(s)");
 		}
 	}
 	public void show (String[] pInputArray) {
 		if(pInputArray.length == 2) {
 			if(_fileSystem.showFile(pInputArray[1])) { //show filename 
 			} else {
-				System.out.println("Error showing file - Probably doesn't exist");
+				System.err.println("Error showing file - Probably doesn't exist");
 			}
 		} else {
-			System.out.println("Invalid parameter");
+		    System.err.println("Invalid parameter(s)");
 		}
 		
 	}
 	public void copyrv (String[] pInputArray){
 		if(pInputArray.length == 3) {
 			if(_fileSystem.copyRealToVirtual(pInputArray[1], pInputArray[2])) { //copyrv realpath virtualpath 
+			    System.out.println("File/directory copied");
 			} else {
-				System.out.println("Error copying file - invalid path or internal error");
+				System.err.println("Error copying file - invalid path or internal error");
 			}
 		} else {
-			System.out.println("Invalid parameter");
+		    System.err.println("Invalid parameter(s)");
 		}
 	}
 	public void copyvr (String[] pInputArray){
 		if(pInputArray.length == 3) { 
-			if(_fileSystem.copyVirtualToReal(pInputArray[1], pInputArray[2])) { //copyvr virtualpath realpath  
+			if(_fileSystem.copyVirtualToReal(pInputArray[1], pInputArray[2])) { //copyvr virtualpath realpath 
+			  System.out.println("File/directory copied");
 			} else {
-				System.out.println("Error copying file - invalid path or internal error");
+				System.err.println("Error copying file - invalid path or internal error");
 			}
 		} else {
-			System.out.println("Invalid parameter");
+		    System.err.println("Invalid parameter(s)");
 		}
 	}
 	public void copyvv (String[] pInputArray){
 		if(pInputArray.length == 3) { 
 			if(_fileSystem.copyVirtualToVirtual(pInputArray[1], pInputArray[2])) { //copyrv realpath virtualpath 
+			    System.out.println("File/directory copied");
 			} else {
-				System.out.println("Error copying file - invalid path or internal error");
+				System.err.println("Error copying file - invalid path or internal error");
 			}
 		} else {
-			System.out.println("Invalid parameter");
+		  System.err.println("Invalid parameter(s)");
 		}
 	}
 	public void move(String[] pInputArray){
@@ -194,18 +201,18 @@ public class MainGUI {
 	      if (_fileSystem.moveDirectory(pInputArray[1], pInputArray[2], newName)) {
 	        System.out.println("Directory moved");
 	      } else {
-	        System.out.println("Error moving directory");
+	        System.err.println("Error moving directory");
 	      }
 	    } else {//Move file
-	      System.out.println(newName);
+	      //System.out.println(newName);
 	      if (_fileSystem.moveFile(pInputArray[1], pInputArray[2], newName)) {
 	        System.out.println("File moved");
 	      } else {
-	        System.out.println("Error moving file");
+	        System.err.println("Error moving file");
 	      }
 	    }
 	  } else {
-	    System.out.println("Invalid parameter");
+	    System.err.println("Invalid parameter(s)");
 	  }
 	}
 	public void remove(String[] pInputArray){
@@ -215,7 +222,7 @@ public class MainGUI {
 	        if (_fileSystem.deleteDirectory(pInputArray[1])) {
 	          System.out.println("Directory removed");
 	        } else {
-	          System.out.println("Error removing directory");
+	          System.err.println("Error removing directory");
 	        }
 	      } else {//Remove file(s)
 	        ArrayList<String> files = new ArrayList<>();
@@ -228,6 +235,8 @@ public class MainGUI {
 	          System.err.println("Error removing file(s)");
 	        }
 	      }
+	  } else {
+	    System.err.println("Invalid parameter(s)");
 	  }
 	}
 	public void find(String[] pInputArray) {
@@ -241,7 +250,7 @@ public class MainGUI {
 		if (pInputArray.length == 1) {
 			_fileSystem.treeFileSystem();
 		} else {
-			System.out.println("Unknown command");
+		    System.err.println("Invalid parameter(s)");
 		}
 	}
 	
@@ -256,7 +265,7 @@ public class MainGUI {
 			String[] inputArray = separeCommand(userInput);
 			switch(inputArray[0].toLowerCase()) {
 				
-				case "help": help(); break;
+				case "help": help(inputArray); break;
 				case "file": file(inputArray); break;
 				case "makedir": makedir(inputArray); break;
 				case "cdir": cdir(inputArray); break;
